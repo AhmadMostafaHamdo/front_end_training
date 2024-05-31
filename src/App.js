a0cfbfe186267dfcc544ca8ec568b41d3b3fe085
AhmadHamdo <ahmadhamdo5123@gmail.com>
2024-05-31 22:30:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import './styles/index.css';
+import './styles/login.css';
+import Init from './website/Init'
+import Logout from './website/Logout'
+import Verfiy from './website/Verfiy'
+import Login from './website/auth/Login'
+import Register from './website/auth/Register'
+
+export default function App() {
+  return (
+       <Routes>
+        <Route path='/verfiy' element={<Verfiy/>}/>
+        <Route path='/' element={<Init/>}/>
+        <Route path='/' element={<Init/>}>
+         <Route path='login' element={<Login/>}/>
+         <Route path='logout' element={<Logout/>}/>
+         <Route path='register' element={<Register/>}/>
+        </Route>
+     </Routes>
+
+  )
+}
